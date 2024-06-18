c70b66bcb267f89f410e24c9866ad6bb9b1dd7db
Satish9047 <satishprajapati930@gmail.com>
2024-06-18 21:06:02 +0545

    image not showing bug fixed

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,47 +1,49 @@
 import { Component } from '@angular/core';
-import { WeatherService } from "../services/weather.service";
-import { CommonModule, NgOptimizedImage } from "@angular/common";
+import { WeatherService } from '../services/weather.service';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
   imports: [CommonModule, NgOptimizedImage],
   templateUrl: './weather.component.html',
-  styleUrl: './weather.component.css'
+  styleUrl: './weather.component.css',
 })
 export class WeatherComponent {
-  myWeather: any
+  myWeather: any;
   temperature: number = 0;
   feels: number = 0;
   pressure: number = 0;
   humidity: number = 0;
-  day: string = "";
-  location: string = "";
-  iconUrl: string = "";
-  icon: string = "";
+  day: string = '';
+  location: string = '';
+  iconUrl: string = '';
+  icon: string = '';
   isLoading: boolean = true;
   isError: boolean = false;
 
-  constructor(
-    private weatherService: WeatherService
-  ) { }
+  constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.weatherService.getWeather("https://api.openweathermap.org/data/2.5/weather?q=Bhaktapur&appid=29a3e1374d13aac7e927cc40513d7719&units=metric").subscribe(
-      {
+    this.weatherService
+      .getWeather(
+        'https://api.openweathermap.org/data/2.5/weather?q=Bhaktapur&appid=29a3e1374d13aac7e927cc40513d7719&units=metric',
+      )
+      .subscribe({
         next: (res) => {
           // console.log(res);
+          // console.log(this.myWeather);
           this.myWeather = res;
-          console.log(this.myWeather);
-          this.day = this.myWeather.weather[0].main
+          this.icon = this.myWeather.weather[0].icon;
+          this.iconUrl =
+            'https://openweathermap.org/img/wn/' + this.icon + '@2x.png';
+          this.day = this.myWeather.weather[0].main;
           this.temperature = this.myWeather.main.temp;
           this.feels = this.myWeather.main.feels_like;
           this.pressure = this.myWeather.main.pressure;
           this.humidity = this.myWeather.main.humidity;
           this.location = this.myWeather.name;
-          this.icon = this.myWeather.weather[0].icon;
-          this.iconUrl = "https://openweathermap.org/img/wn/" + this.icon + "@2x.png";
         },
         error: (error) => {
           this.isLoading = false;
@@ -50,11 +52,8 @@ export class WeatherComponent {
         },
         complete: () => {
           this.isLoading = false;
-          console.log("Completed");
-        }
-      }
-    )
-
+          console.log('Completed');
+        },
+      });
   }
 }
-
